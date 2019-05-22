38bcd3c0f6eb64881180152a71a801013b72a5db
mostrovski <mail@mostrovski.com>
2019-05-22 14:49:00 +0200

    Get rid of magic in Page class

--- core/Page.php
+++ core/Page.php
@@ -21,19 +21,19 @@ class Page {
 
         if (!isset($_GET['url'])) return;
 
-        $url = self::transform($_GET['url']);
+        $requested = self::transform($_GET['url']);
 
-        if (!in_array($url[0], $public) &&
-            !in_array($url[0], $private)) {
+        if (!in_array($requested['resource'], $public) &&
+            !in_array($requested['resource'], $private)) {
             return;
         }
 
-        if (!$loggedIn && in_array($url[0], $private)) {
+        if (!$loggedIn && in_array($requested['resource'], $private)) {
             $_SESSION['flash'] = AUTHENTICATION_ERROR;
             return Helper::redirect(HOME);
         }
 
-        self::parse($url);
+        self::parse($requested);
     }
 
     protected static function instantiate($controller) {
@@ -44,27 +44,31 @@ class Page {
         call_user_func_array([$controller, $method], $params);
     }
 
-    protected static function parse($url) {
-        // Map url parts to controllers, methods, and parameters
-        $controller = 'Controllers\\'.ucfirst($url[0]).'Controller';
+    protected static function parse($requested) {
+        // Map requested url to controllers, methods, and parameters
+        $controller = 'Controllers\\'.ucfirst($requested['resource']);
+        $controller .= 'Controller';
         self::$controller = $controller;
-        unset($url[0]);
 
-        if (!isset($url[1])) return;
-        if (!method_exists(self::$controller, $url[1])) return;
+        if (!isset($requested['method']) ||
+            !method_exists(self::$controller, $requested['method']))
+        return;
 
-        self::$method = $url[1];
-        unset($url[1]);
-
-        self::$params = $url ? array_values($url) : [];
+        self::$method = $requested['method'];
+        self::$params = $requested['parameters'];
     }
 
     protected static function transform($url) {
-        // Turn the url into an array
+        // Turn the url into an associative array
         $url = rtrim($url, '/');
         $url = filter_var($url, FILTER_SANITIZE_URL);
         $url = explode('/', $url);
-        return $url;
+
+        return [
+            'resource'   => $url[0],
+            'method'     => $url[1] ?? NULL,
+            'parameters' => array_slice($url, 2),
+        ];
     }
 }
 ?>
