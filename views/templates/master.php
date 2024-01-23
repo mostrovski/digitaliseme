26d2afa9cdc8678889bc9dc5876da24aa345aa4b
mostrovski <ostrovskiime@gmail.com>
2024-01-23 13:49:47 +0100

    Add favicon

--- views/templates/master.php
+++ views/templates/master.php
@@ -6,6 +6,10 @@
         <?= config('app.info.name').' | '.($metaTitle ?? '') ?>
     </title>
     <link rel="stylesheet" href="<?= url('css/main.css') ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= url('img/icons/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= url('img/icons/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= url('img/icons/favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?= url('site.webmanifest') ?>">
 </head>
 <body>
     <?php include_once app()->root().'/views/partials/header.php'; ?>
